f0a13f0361828fd73f1ad8102b980a854ed0cdd3
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-02-22 20:50:04 -0800

    DONE

---
+++ README.md
@@ -0,0 +1,76 @@
+# App to Keep Track of Books
+
+## Tech Used
+- Python
+- SQL
+- HTML/CSS
+- Jinja2
+- click
+- Flask
+- Flask-SQLAlchemy
+- Flask-WTF
+- itsdangerous
+- MarkupSafe
+- python-dotenv
+- SQLAlchemy
+- Werkzeug
+- WTForms
+- unittest
+
+## Setup/Install
+
+```
+pip3 install -r requirements.txt
+cp .env.example .env
+python3 app.py
+```
+
+***Tests With***
+
+```
+python3 -m unittest discover
+```
+
+(Make sure you have unittest installed.)
+
+***To run all tests from a single file***
+
+```
+python3 -m unittest books_app.main.tests
+```
+
+***To run one specific test***
+
+```
+python3 -m unittest books_app.main.tests.MainTests.test_homepage_logged_in
+```
+
+## Img
+
+***Home***
+
+![Image of main page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/home.png)
+
+***Sign Up***
+
+![Image of sign up page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/sign-up.png)
+
+***Log In***
+
+![Image of Log In page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/log-in.png)
+
+***Profile***
+
+![Image of profile page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/profile.png)
+
+***Create Book***
+
+![Image of Create Book page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/create-book.png)
+
+***Create Author***
+
+![Image of Create Author page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/create-author.png)
+
+***Create Genre***
+
+![Image of Create Genre page](https://raw.githubusercontent.com/LWRGitHub/app-to-keep-track-of-books/master/assets/create-genre.png)
---
+++ books_app/auth/tests.py
@@ -0,0 +1,163 @@
+import os
+from unittest import TestCase
+
+from datetime import date
+ 
+from books_app import app, db, bcrypt
+from books_app.models import Book, Author, User, Audience
+
+"""
+Run these tests with the command:
+python -m unittest books_app.main.tests
+"""
+
+#################################################
+# Setup
+#################################################
+
+def create_books():
+    a1 = Author(name='Harper Lee')
+    b1 = Book(
+        title='To Kill a Mockingbird',
+        publish_date=date(1960, 7, 11),
+        author=a1
+    )
+    db.session.add(b1)
+
+    a2 = Author(name='Sylvia Plath')
+    b2 = Book(title='The Bell Jar', author=a2)
+    db.session.add(b2)
+    db.session.commit()
+
+def create_user():
+    password_hash = bcrypt.generate_password_hash('password').decode('utf-8')
+    user = User(username='me1', password=password_hash)
+    db.session.add(user)
+    db.session.commit()
+
+
+#################################################
+# Tests
+#################################################
+
+class AuthTests(TestCase):
+    """Tests for authentication (login & signup)."""
+
+    def setUp(self):
+        """Executed prior to each test."""
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    def test_signup(self):
+        # tests for the signup route. It should:
+        # - Make a POST request to /signup, sending a username & password
+        post_data = {
+            'username': 'Some Name',
+            'password': '000'
+        }
+        self.app.post('/signup', data=post_data)
+
+        # - Check that the user now exists in the database
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn('Some Name', response_text)
+       
+
+    def test_signup_existing_user(self):
+        # Tests for the signup route. It should:
+        # - Create a user
+        post_data = {
+            'username': 'Some Name',
+            'password': '000'
+        }
+        self.app.post('/signup', data=post_data)
+
+        # - Make a POST request to /signup, sending the same username & password
+        post_data = {
+            'username': 'Some Name',
+            'password': '000'
+        }
+        response = self.app.post('/signup', data=post_data)
+
+        # - Check that the form is displayed again with an error message
+        response_text = response.get_data(as_text=True)
+        self.assertIn('That username is taken. Please choose a different one.', response_text)
+
+    def test_login_correct_password(self):
+        # Tests for the login route. It should:
+        # - Create a user
+        create_user()
+
+        # - Makes a POST request to /login, sending the created username & password
+        post_data = {
+            'username': 'me1',
+            'password': 'password'
+        }
+        self.app.post('/login', data=post_data)
+
+        # - Checks that the user's name is now displayed on the homepage
+        response = self.app.get('/', follow_redirects=True)
+
+        # - Check that the form is displayed again with an error message
+        response_text = response.get_data(as_text=True)
+        self.assertIn('me1', response_text)
+
+    def test_login_nonexistent_user(self):
+        # Test for the login route. It should:
+        # - Make a POST request to /login, sending a username & password
+        post_data = {
+            'username': 'Some Name',
+            'password': 'password'
+        }
+        response = self.app.post('/login', data=post_data)
+        # - Check that the login form is displayed again, with an appropriate
+        #   error message
+        response_text = response.get_data(as_text=True)
+        self.assertIn('No user with that username. Please try again.', response_text)
+        
+
+    def test_login_incorrect_password(self):
+        # Tests for the login route. It should:
+        # - Create a user
+        create_user()
+
+        # - Make a POST request to /login, sending the created username &
+        #   an incorrect password
+        post_data = {
+            'username': 'me1',
+            'password': '000'
+        }
+        response = self.app.post('/login', data=post_data)
+
+        # - Checks that the login form is displayed again, with an appropriate
+        #   error message
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Password doesn&#39;t match. Please try again.", response_text)
+
+    def test_logout(self):
+        # Test for the logout route. It should:
+        # - Create a user
+        create_user()
+
+        # - Logs the user in (make a POST request to /login)
+        post_data = {
+            'username': 'me1',
+            'password': 'password'
+        }
+        self.app.post('/login', data=post_data)
+
+        # - Make a GET request to /logout
+        self.app.get('/logout', data=post_data)
+
+        # - Check that the "log in" button is showing
+        response = self.app.get('/', follow_redirects=True)
+        
+        response_text = response.get_data(as_text=True)
+        self.assertNotIn('Log Out', response_text)
